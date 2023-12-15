1ac02797dcf8efe4f2ae4b8e5a427ca2e0ff5673
Rik Huijzer <github@huijzer.xyz>
2023-12-15 16:15:50 +0100

    Download repository with cache

--- mlir-dialects.py
+++ mlir-dialects.py
@@ -1,8 +1,34 @@
 import argparse
+import os
+import subprocess
+
+from pathlib import Path
+
+def clone_or_update_repo(git_url: str, cache_dir: str):
+    """Clone or update a git repository."""
+    repo_name = git_url.split('/')[-1]
+
+    assert repo_name.endswith('.git')
+    repo_name = repo_name[:-4]
+
+    local_repo_path = os.path.join(cache_dir, repo_name)
+
+    if os.path.isdir(local_repo_path):
+        print(f"Updating repository: {repo_name}")
+        subprocess.run(["git", "-C", local_repo_path, "pull"], check=True)
+    else:
+        print(f"Cloning repository: {repo_name}")
+        subprocess.run(["git", "clone", "--depth", "1", git_url, local_repo_path], check=True)
 
 
 def count():
     print("Counting...")
+    urls = [
+        "https://github.com/llvm/torch-mlir.git",
+    ]
+    cache_dir = str(Path.home() / ".cache" / "mlir-dialects")
+    for url in urls:
+        clone_or_update_repo(url, cache_dir)
 
 
 def main():
